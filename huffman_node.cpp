ef9dd9fa435263cdb1df9120600e3b53b168ff19
tesmattar <thiago.mattar.163@ufrn.edu.br>
2025-10-13 11:16:01 -0300

    Update huffman_node.cpp
    ajuste de acordo com as mudanças no huffman_node.h

--- huffman_node.cpp
+++ huffman_node.cpp
@@ -1,9 +1,8 @@
-// huffman_tree.cpp
 #include "huffman_node.h"
 #include <queue>
 #include <unordered_map>
 
-HuffmanNode* buildHuffmanTree(const std::unordered_map<char, int>& freqMap) {
+HuffmanNode* buildHuffmanTree(const std::unordered_map<std::string, int>& freqMap) {
     std::priority_queue<HuffmanNode*, std::vector<HuffmanNode*>, Compare> pq;
 
     for (auto [symbol, freq] : freqMap)
@@ -14,8 +13,8 @@ HuffmanNode* buildHuffmanTree(const std::unordered_map<char, int>& freqMap) {
         HuffmanNode* right = pq.top(); pq.pop();
 
         int combinedFreq = left->freq + right->freq;
-        pq.push(new HuffmanNode('\0', combinedFreq, left, right));
+        pq.push(new HuffmanNode("", combinedFreq, left, right));
     }
 
-    return pq.top(); // raiz
+    return pq.top(); 
 }

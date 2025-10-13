89be35fcc6f2e8a1870ee931cfc4f6744a012e6c
tesmattar <thiago.mattar.163@ufrn.edu.br>
2025-10-13 11:08:28 -0300

    Update huffman_node.h
    atualizando o codigo para usar strings igual é usado no FreqCounter ao inves de char

--- huffman_node.h
+++ huffman_node.h
@@ -1,22 +1,23 @@
-// huffman_node.h
 #ifndef HUFFMAN_NODE_H
 #define HUFFMAN_NODE_H
 
+#include <string>
+
 struct HuffmanNode {
-    char symbol;
+    std::string symbol;  // agora é string, não char
     int freq;
     HuffmanNode *left;
     HuffmanNode *right;
 
-    HuffmanNode(char s, int f, HuffmanNode* l = nullptr, HuffmanNode* r = nullptr)
+    HuffmanNode(const std::string& s, int f, HuffmanNode* l = nullptr, HuffmanNode* r = nullptr)
         : symbol(s), freq(f), left(l), right(r) {}
 };
 
-
 struct Compare {
-    bool operator()(HuffmanNode* a, HuffmanNode* b) {
-        return a->freq > b->freq; 
+    bool operator()(HuffmanNode* a, HuffmanNode* b) const {
+        return a->freq > b->freq;
     }
 };
 
 #endif
+
